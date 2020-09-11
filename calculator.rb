911e87afac940e90b30d232d683e070221bc33e1
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-11 22:54:46 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
 # Add your variables here
+first_number = 57
+second_number = 13
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
